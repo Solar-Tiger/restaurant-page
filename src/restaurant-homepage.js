48d248c35ee8a60e37b893c35e5f00d3f3c07e95
David Singleton <davidjsingleton18@gmail.com>
2024-05-17 10:34:21 -0700

    Update homepage to display new logo in place of placeholder

---
+++ src/restaurant-homepage.js
@@ -0,0 +1,27 @@
+import tacoImage from './assets/images/chaos-kitchen-logo.png';
+
+export default function loadRestaurantHomepage() {
+  const homepageContent = document.getElementById('content');
+
+  const containerDiv = document.createElement('div');
+
+  homepageContent.textContent = '';
+
+  const h1 = document.createElement('h1');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+
+  h1.textContent = "Welcome to Random's Chaos Kitchen!";
+
+  img.src = tacoImage;
+  img.width = '340';
+  img.height = '190';
+  img.alt = 'chaos tacos';
+
+  p.textContent =
+    "You won't find more chaos and mayhem then here at the Chaos Kitchen! We whip you up storms of meals that even the gods themselves would find hard to quell!";
+
+  containerDiv.append(h1, img, p);
+
+  homepageContent.appendChild(containerDiv);
+}
